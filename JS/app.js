1f77b900eb08e245651d899bf1b294ffdcdd1a08
fidanrehimli <161814776+fidanrehimli@users.noreply.github.com>
2024-04-02 00:22:41 -0700

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,88 @@
+// const num1 = 3;
+// const num2 = 5;
+// if (num1 >num2) {
+//     console.log("num1 is greater than num2")
+// }
+// else if (num2> num1) {
+//     console.log ("num2 is bigger than num1")
+// }
+// else{
+//     console.log();
+// }
+
+// const a = 10;
+// const b = 50;
+// const c = 30;
+// if (c<a+b && a<b+c && b<a+c)  {
+//     console.log ("yes")
+// }
+// else{
+//     console.log("no")
+// }
+
+
+// const a = 6;
+// const b = 8;
+// const c = 10;
+// if (c**2 == a**2 + b**2) {
+//     console.log ("yes")
+// }
+// else{
+//     console.log("no")
+// }
+
+
+// const a = 40;
+// if (a > 40){
+//     console.log("error")
+// }
+// else {
+//     console.log("welcome")
+// }
+
+
+// const a = 50;
+// if (a > 40){
+//     console.log("welcome")
+// }
+// else {
+//     console.log("error")
+// }
+// console.log(a-40);
+
+
+// const a = 50;
+// if (a > 60){
+//     console.log("welcome")
+// }
+// else {
+//     console.log("Balansda yeterli mebleg yoxdur", a-40  )
+// }
+
+// let operation = 10;
+// let newBalance = 50;
+
+// switch (operation) {
+//     case 10:
+//         if (operation >= 40){
+//                 console.log("welcome" , operation % 40)
+//             }
+//             else if (operation<40 && operation%40){
+//                 console.log("Balansda yeterli mebleg yoxdur", 40% operation )
+//             }
+//         break;
+//         case 40:
+//             console.log ("yeni balance" , operation + newBalance)
+//             break;
+//         }
+
+
+
+
+
+
+
+
+
+
+
